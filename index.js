4a26d9214ad260b138df33ea15325bc3aa959191
Jeff Stern <jeffstern.ny@gmail.com>
2022-04-18 13:56:47 -0400

    working on prob function

---
+++ index.js
@@ -0,0 +1,14 @@
+let turf = require('@turf/turf');
+
+function generateProbabilities(originsGeom, destinationsGeom, { distanceThreshold = 10, optionTwo = 'hello' } = {}) {
+  console.log(originsGeom);
+  console.log(`Huff Model for originsGeom and destinationGeom; 
+  distance Threshold is ${distanceThreshold} and option2 is ${optionTwo}`);
+}
+
+function Huff() {
+  return 'huff model';
+}
+
+module.exports = Huff;
+Huff.generateProbabilities = generateProbabilities;
